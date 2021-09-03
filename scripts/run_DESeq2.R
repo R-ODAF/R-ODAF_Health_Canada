46869a856b82efa1c21b74a71daa8eb11073591f
Kate Cook <kate.cook@gmail.com>
2021-09-03 09:38:53 -0400

    set up paths

--- scripts/run_DESeq2.R
+++ scripts/run_DESeq2.R
@@ -20,12 +20,23 @@ DESeqDesign <- read.delim(SampleKeyFile,
                           row.names = 1) # Column must have unique IDs!!
 DESeqDesign$original_names <- rownames(DESeqDesign)
 
+paths <- set_up_paths(params)
 
+# Identify where count data can be found
+if (Platform == "TempO-Seq") {
+  SampleDataFile <- file.path(paths$processed, "count_table.csv")
+  sampledata_sep = ","
+} else {
+  SampleDataFile <- file.path(paths$processed, "genes.data.tsv")
+  sampledata_sep = "\t"
+}
 
 
 if (is.na(config$params$group_facet)) { # all data in one facet
   message(paste0("Making multiple reports based on ",
                  config$params$group_facet ,"..."))
+    # load data
+    # run DEseq2
 
 } else {
     facets <- DESeqDesign %>%
@@ -43,4 +54,28 @@ if (is.na(config$params$group_facet)) { # all data in one facet
         message("Writing a single report for whole experiment.")
 
     }
+}
+
+
+set_up_paths <- function(params) {
+    paths <- list()
+    # Other important system paths to specify in config
+    paths$wikipathways <- params$wikipathways_directory
+    # For project structure
+    # Should probably update this to use the file.path() function.
+    paths$root <- params$projectdir
+    paths$data <- file.path(paths$root, "data")
+      paths$raw <- file.path(paths$data, "raw")
+      paths$processed <- file.path(paths$data, "processed")
+      paths$metadata <- file.path(paths$data, "metadata")
+    paths$reports <- file.path(paths$root, "reports")
+    paths$results <- file.path(paths$root, "results")
+    if (is.na(params$group_facet)) {
+      paths$DEG_output <- file.path(paths$results, "DEG_output")
+    } else {
+      paths$DEG_output <- file.path(paths$results, "DEG_output", paste0("group_", paste(params$group_filter, collapse = "_")))
+    }
+    paths$pathway_analysis <- file.path(paths$DEG_output, "/pathway_analysis")
+    paths$RData <- file.path(paths$DEG_output, "/RData")
+    paths$BMD_output <- file.path(paths$results, "/DEG_output/BMD_and_biomarker_files")
 }
