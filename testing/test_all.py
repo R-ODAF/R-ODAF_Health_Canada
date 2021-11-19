248a5e4d1bbe16c4f23793014bc18c627ec8e01a
Kate Cook <kate.cook@gmail.com>
2021-11-19 13:13:59 -0500

    use python unittest

--- testing/test_all.py
+++ testing/test_all.py
@@ -1,31 +1,47 @@
+import unittest
 import os
+import subprocess
 
 run_pipeline = True
-run_QC = True
-run_deseq = True
+run_QC = False
+run_deseq = False
 
-cwd = os.getcwd()
-print(cwd)
+# cwd = os.getcwd()
+# print(cwd)
 
-os.chdir("..") # should be main dir
+# os.chdir("..") # should be main dir
 
-cwd = os.getcwd()
-print(cwd)
+# cwd = os.getcwd()
+# print(cwd)
+
+class Tester(unittest.TestCase):
+
+    def test_pipeline(self):
+        if(run_pipeline):
+            # run snakemake pipeline
+            p = subprocess.Popen('snakemake -s workflow/Snakefile.temposeq --cores=1 -R all --delete-all-output', shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+            p.communicate()
+            p = subprocess.Popen('snakemake -s workflow/Snakefile.temposeq --cores=1 all', shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+            p.communicate()
+            
+            # Check exits code and other expected output            
+            self.assertEqual(0, p.returncode)
+            #self.assertTrue(os.path.isfile('some.pipeline.output.txt'))
 
 
-# run snakemake pipeline
-os.system('snakemake --cores=1 -s workflow/Snakefile.temposeq -R all_temposeq')
+    def test_QC(self):    # run snakemake pipeline
+        if(run_deseq):
+            # run QC
+            os.system('Rscript scripts/render_studywide_QC_report.R')
 
-# check pipeline output
+            # check QC output
 
+    def test_deseq(self):
+        if(run_deseq):
+            # run DEseq2
+            os.system('Rscript scripts/render_DESeq2_report.R')
 
-# run QC
-os.system('Rscript scripts/render_studywide_QC_report.R')
+            # check DEseq2 output
 
-# check QC output
-
-
-# run DEseq2
-os.system('Rscript scripts/render_DESeq2_report.R')
-
-# check DEseq2 output
+if __name__ == '__main__':
+    unittest.main()
