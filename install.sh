fce532debd24dcdfeb3ebca18f3db7e25da7876c
mattjmeier <matthew.james.meier@gmail.com>
2021-12-08 10:21:33 -0500

    Automatically get the project root directory and populate the config file

--- install.sh
+++ install.sh
@@ -1,1 +1,7 @@
-cp config/config.default.yaml config/config.yaml
+#!/usr/bin/env bash
+# Find directory where script is running from
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+# Copy default configuration to config.yaml
+cp ${SCRIPT_DIR}/config/config.default.yaml ${SCRIPT_DIR}/config/config.yaml
+# Modify the config file to update project root directory
+sed -i 's:prefix.*$:prefix\: '${SCRIPT_DIR}'/:' config/config.yaml
