29925ad866cc126d8519fcc153fbd06da77007d0
mattjmeier <matthew.james.meier@gmail.com>
2022-02-14 09:34:16 -0500

    Modify DEG summary figure

--- scripts/utilities/DEG_summary_plot.R
+++ scripts/utilities/DEG_summary_plot.R
@@ -1,6 +1,9 @@
 # Find DEG tables
 
-files <- fs::dir_ls("./analysis/DEG_lists/", regexp = "DEG_summary.txt",
+library(ggplot2)
+library(tidyverse)
+
+files <- fs::dir_ls("./analysis_0.1_distance/DEG_lists/", regexp = "DEG_summary.txt",
                     recurse = T)
 files <- as.vector(files)
 
@@ -31,14 +34,60 @@ degs <- files %>%
 # Some fun regex if you want to split things up...
 # This takes the last space in the string as the delimiter
 degs <- degs %>% tidyr::separate(Sample, c("Chemical","Timepoint"),
-                                 sep = "\\s+(?=\\S*$)" )
+                                 sep = "_" )
 #
 ###################################################
 
 ###################################################
 # Plot
-ggplot(degs, aes(x = Condition, y = DEGs, fill = Timepoint)) +
+pctdist10 <- ggplot(degs, aes(x = Condition, y = DEGs, fill = Timepoint)) +
   geom_bar(stat="identity", position="dodge") +
   facet_wrap(~Chemical, scales = "free_x") +
   theme(axis.text.x = element_blank()) # element_text(angle = 90))
+
+
+
+
+files <- fs::dir_ls("./analysis_0.15_distance/DEG_lists/", regexp = "DEG_summary.txt",
+                    recurse = T)
+files <- as.vector(files)
+
+# Exclude the top-level summary...
+files <- files[grep("DEG_lists/DEG_summary", files, invert = T)]
+
+# Function to read in one  at a time and wrangle data
+clean_data <- function(x) {
+  df <- read.table(x, sep = "\t", header = F, fill = T)  
+  df <- as.data.frame(t(df))
+  df[1,1] <- "Condition"
+  colnames(df) <- df[1,]
+  df <- df[2:nrow(df),]
+  # subset - shouldn't be needed but it is?
+  df <- df[,1:2]
+  final <-  reshape2::melt(df, id.vars=1)
+  colnames(final) <- c("Condition","Sample","DEGs")
+  final$DEGs <- as.numeric(final$DEGs)
+  final$Condition <- factor(final$Condition, levels = final$Condition)
+  return(final)
+}
+
+degs <- files %>%
+  map_dfr(clean_data)
+
+###################################################
+# Optional... Data cleaning examples
+# Some fun regex if you want to split things up...
+# This takes the last space in the string as the delimiter
+degs <- degs %>% tidyr::separate(Sample, c("Chemical","Timepoint"),
+                                 sep = "_" )
+#
+###################################################
+
+###################################################
+# Plot
+pctdist15 <- ggplot(degs, aes(x = Condition, y = DEGs, fill = Timepoint)) +
+  geom_bar(stat="identity", position="dodge") +
+  facet_wrap(~Chemical, scales = "free_x") +
+  theme(axis.text.x = element_blank()) # element_text(angle = 90))
+
 
