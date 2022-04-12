0f6242bbce0da27e454a1a43e370698e091788b3
Kate Cook <kate.cook@gmail.com>
2022-04-12 14:02:53 -0400

    python script to concatenate fastqs

--- concat_fastqs.py
+++ concat_fastqs.py
@@ -1,9 +1,18 @@
+import pandas as pd
+import os
+
 indir = "/mnt/nextseqData/Runs"
-indirs = ["220331_NB501400_0164_AHTWJ2BGXK_FASTQ", "220406_NB501400_0165_AHTWC7BGXK_FASTQ", "220407_NB501400_0166_AHTYG5BGXK_FASTQ", "220408_NB501400_0167_AHTWHGBGXK_FASTQ", "220411_NB501400_0168_AHTW7LBGXK_FASTQ"] 
+indirs = ["220331_NB501400_0164_AHTWJ2BGXK_FASTQ/Dechlorane_Plus", "220406_NB501400_0165_AHTWC7BGXK_FASTQ", "220407_NB501400_0166_AHTYG5BGXK_FASTQ", "220408_NB501400_0167_AHTWHGBGXK_FASTQ", "220411_NB501400_0168_AHTW7LBGXK_FASTQ"] 
 
+outdir = "data/raw"
 
+metadata = pd.read_csv("data/metadata/metadata.csv")
 
-
-for i in indirs: 
-    for file in NA24694_GCCAAT_L001_R${i}_*fastq.gz:
-        cat "$file" >> EA00694_GCCAAT_L001_R${i}.fastq.gz && rm "$file"
+for sample in metadata["Sample_ID"]:
+    outfile = outdir + "/" + sample + ".fastq.gz"
+    print(outfile)
+    #os.system("rm {}".format(outfile))
+    for i in indirs:
+        file = indir + "/" + i + "/" + sample + "_S*_R1_001.fastq.gz"
+        print(file)
+        #os.system("cat {} >> {}".format(file, outfile))
