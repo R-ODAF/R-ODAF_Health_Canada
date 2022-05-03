088bfa79d6cceb08adc6a9801f587f9549f17ca4
Kate Cook <kate.cook@gmail.com>
2022-05-03 10:41:04 -0400

    updates

--- scripts/render_DESeq2_report.R
+++ scripts/render_DESeq2_report.R
@@ -8,8 +8,11 @@
 # Sys.setenv(RSTUDIO_PANDOC="OUTPUT FROM ABOVE COMMAND")
 # Sys.setenv(RSTUDIO_PANDOC="/usr/lib/rstudio-server/bin/pandoc")
 
-library(tidyverse)
-require(yaml)
+library('tidyverse')
+library('devtools')
+library('DESeq2')
+library('data.table')
+library('yaml')
 
 source(here::here("scripts","file_functions.R"))
 source(here::here("scripts","setup_functions.R"))
@@ -39,6 +42,7 @@ bs <- params$bs
 params <- set_up_platform_params(params, bs, species_data)
 
 skip_extra <- c("DMSO") # Remove DMSO controls as a facet
+digits = 2 # For rounding numbers
 
 
 # Identify where metadata can be found
@@ -64,28 +68,28 @@ if (!dir.exists(deglist_dir)) {dir.create(deglist_dir, recursive = TRUE)}
 make_reports <- function(file_prefix,pars){
   # Input files - Rmd
   # main_report <- file.path(projectdir, "Rmd", "DESeq2_report_new.Rmd")
-  extra_stats_report <- file.path(projectdir, "Rmd", "extra_stats_report.Rmd")
-  # data_explorer_report <- file.path(projectdir, "Rmd", "data_explorer_report.Rmd")
+  # extra_stats_report <- file.path(projectdir, "Rmd", "extra_stats_report.Rmd")
+  data_explorer_report <- file.path(projectdir, "Rmd", "data_explorer_report.Rmd")
   
   # main_file <-          file.path(report_dir, paste0(file_prefix,".html"))
-  extra_stats_file <-   file.path(report_dir, paste0("extra_stats_",file_prefix,".html"))
-  # data_explorer_file <- file.path(report_dir, paste0("data_explorer_",file_prefix,".html"))
+  # extra_stats_file <-   file.path(report_dir, paste0("extra_stats_",file_prefix,".html"))
+  data_explorer_file <- file.path(report_dir, paste0("data_explorer_",file_prefix,".html"))
   
   # rmarkdown::render(input = main_report,
   #                   encoding = "UTF-8",
   #                   output_file = main_file,
   #                   params = pars,
   #                   envir = new.env())
-  rmarkdown::render(input = extra_stats_report,
-                    encoding = "UTF-8",
-                    output_file = extra_stats_file,
-                    params = pars,
-                    envir = new.env())
-  # rmarkdown::render(input = data_explorer_report,
+  # rmarkdown::render(input = extra_stats_report,
   #                   encoding = "UTF-8",
-  #                   output_file = data_explorer_file,
+  #                   output_file = extra_stats_file,
   #                   params = pars,
   #                   envir = new.env())
+  rmarkdown::render(input = data_explorer_report,
+                    encoding = "UTF-8",
+                    output_file = data_explorer_file,
+                    params = pars,
+                    envir = new.env())
 }
 
 
