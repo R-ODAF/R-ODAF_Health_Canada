294d59f927f4c342ae7fed73e6e351d4c7f2aab5
Kate Cook <kate.cook@gmail.com>
2022-06-02 09:26:07 -0400

    add options to generate (or not) each report

--- scripts/render_DESeq2_report.R
+++ scripts/render_DESeq2_report.R
@@ -66,36 +66,46 @@ if (!dir.exists(deglist_dir)) {dir.create(deglist_dir, recursive = TRUE)}
 
 # convenience function
 make_reports <- function(file_prefix,pars){
-  main_report <- file.path(projectdir, "Rmd", "DESeq2_report_new.Rmd")
-  # extra_stats_report <- file.path(projectdir, "Rmd", "extra_stats_report.Rmd")
-  # data_explorer_report <- file.path(projectdir, "Rmd", "data_explorer_report.Rmd")
-  # go_pathway_report <- file.path(projectdir, "Rmd", "go_pathway_report.Rmd")
-  
-  main_file <-          file.path(report_dir, paste0(file_prefix,".html"))
-  # extra_stats_file <-   file.path(report_dir, paste0("extra_stats_",file_prefix,".html"))
-  # data_explorer_file <- file.path(report_dir, paste0("data_explorer_",file_prefix,".html"))
-  # go_pathway_file <- file.path(report_dir, paste0("go_pathway_",file_prefix,".html"))
-  
-  rmarkdown::render(input = main_report,
-                    encoding = "UTF-8",
-                    output_file = main_file,
-                    params = pars,
-                    envir = new.env())
-  # rmarkdown::render(input = extra_stats_report,
-  #                   encoding = "UTF-8",
-  #                   output_file = extra_stats_file,
-  #                   params = pars,
-  #                   envir = new.env())
-  # rmarkdown::render(input = data_explorer_report,
-  #                   encoding = "UTF-8",
-  #                   output_file = data_explorer_file,
-  #                   params = pars,
-  #                   envir = new.env())
-  # rmarkdown::render(input = go_pathway_report,
-  #                   encoding = "UTF-8",
-  #                   output_file = go_pathway_file,
-  #                   params = pars,
-  #                   envir = new.env())
+  if(params$generate_main_report){
+    main_report <- file.path(projectdir, "Rmd", "DESeq2_report_new.Rmd")
+    main_file <- file.path(report_dir, paste0(file_prefix,".html"))
+    rmarkdown::render(input = main_report,
+                      encoding = "UTF-8",
+                      output_file = main_file,
+                      params = pars,
+                      envir = new.env())
+    
+  }
+  if(params$generate_extra_stats_report){
+    extra_stats_report <- file.path(projectdir, "Rmd", "extra_stats_report.Rmd")
+    extra_stats_file <- file.path(report_dir, paste0("extra_stats_",file_prefix,".html"))
+    rmarkdown::render(input = extra_stats_report,
+                      encoding = "UTF-8",
+                      output_file = extra_stats_file,
+                      params = pars,
+                      envir = new.env())
+    
+  }
+  if(params$generate_data_explorer_report){
+    data_explorer_report <- file.path(projectdir, "Rmd", "data_explorer_report.Rmd")
+    data_explorer_file <- file.path(report_dir, paste0("data_explorer_",file_prefix,".html"))  
+    rmarkdown::render(input = data_explorer_report,
+                      encoding = "UTF-8",
+                      output_file = data_explorer_file,
+                      params = pars,
+                      envir = new.env())
+    
+  }
+  if(params$generate_go_pathway_report){
+    go_pathway_report <- file.path(projectdir, "Rmd", "go_pathway_report.Rmd")
+    go_pathway_file <- file.path(report_dir, paste0("go_pathway_",file_prefix,".html"))
+    rmarkdown::render(input = go_pathway_report,
+                      encoding = "UTF-8",
+                      output_file = go_pathway_file,
+                      params = pars,
+                      envir = new.env())
+    
+  }
 }
 
 
