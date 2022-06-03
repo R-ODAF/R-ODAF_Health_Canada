20725f9eef1be04fe406f43329fc6ae2566768be
mattjmeier <matthew.james.meier@gmail.com>
2022-06-03 09:56:40 -0400

    Clean up temposeq quantification script; remove mapped/unmapped legacy code

--- scripts/temposeq/temposeq_quantification.R
+++ scripts/temposeq/temposeq_quantification.R
@@ -16,35 +16,23 @@ num_threads   <- as.numeric(args[6])
 cl <- makeCluster(num_threads)
 
 # set up cache dir in tempdir
-cache_dir <- paste0(tempdir(),"/proba")
+cache_dir <- paste0(tempdir(), "/proba")
 dir.create(cache_dir)
 
 message("Running qAlign function...")
 proj2 <- qAlign(samplefile,
                 genome,
-                paired="no",
-                cacheDir=cache_dir,
-                clObj=cl)
+                paired = "no",
+                cacheDir = cache_dir,
+                clObj = cl)
 
-txStart <- import.gff(annotfile, format="gtf")
+txStart <- import.gff(annotfile, format = "gtf")
 names(txStart) <- txStart@seqnames
 
 cnt <- qCount(proj2, txStart, clObj = cl)
 
 # Make dataframe as count table
-cnmat           <- as.data.frame(cnt, header=TRUE)
+cnmat           <- as.data.frame(cnt, header = TRUE)
 cnmat$width     <- NULL
-write.table(cnmat, count_table_file, sep='\t', quote=F, row.names=T)
-cat("Counts Table Completed\n")
-
-# Get Mapped/Unmapped Counts and Output
-a_frame     <- data.frame(alignmentStats(proj2), header=TRUE, check.names=FALSE) 
-unmapped    <- a_frame$unmapped 
-mapped      <- colSums(cnmat) 
-two         <- rbind(mapped) 
-three       <- data.frame(two, check.names=FALSE)     
-three$width <- NULL 
-ll          <- rbind(unmapped=unmapped, mapped=three)
-
-write.table(as.matrix(ll), mapped_unmapped_file, sep='\t', quote=F, row.names=T)
-message("Mapped/Unmapped Table Completed\n")
+write.table(cnmat, count_table_file, sep = '\t', quote = F, row.names = T)
+message("Counts Table Completed")
