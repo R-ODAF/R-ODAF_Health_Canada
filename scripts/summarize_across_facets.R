930c4d03b0bf1845c114cfa828dc57a247dc202f
Kate Cook <kate.cook@gmail.com>
2022-06-30 11:29:24 -0400

    sort summarization plots by contrasts

--- scripts/summarize_across_facets.R
+++ scripts/summarize_across_facets.R
@@ -3,6 +3,7 @@ library('devtools')
 library('DESeq2')
 library('data.table')
 library('yaml')
+library('stringr')
 
 
 # load params
@@ -40,11 +41,14 @@ load(params$dataFile) # metadata, contrasts, counts, resultsList
 
 allResultsUnfaceted <- data.frame()
 significantResultsUnfaceted <- data.frame()
-FilterStatsUnfaceted <- data.frame()
+
+ordered_contrasts <- paste0(contrasts$V1, " vs ", contrasts$V2)
+ordered_levels <- expand.grid(facets,ordered_contrasts) %>% arrange(Var1) %>% mutate(asdf = paste0(Var1, ": ", Var2)) %>% pull(asdf)
 
 for (f in facets){
   resultsListAll <- overallResListAll[[f]]
   resultsListDEGs <- overallResListDEGs[[f]]
+  design <- designList[[f]]
 
   allResults <- annotate_deseq_table(resultsListAll, params, filter_results = F)
   significantResults <- annotate_deseq_table(resultsListDEGs, params, filter_results = F)
@@ -58,37 +62,43 @@ prefix <- paste0(params$platform, "_",
                  paste(params$current_filter, collapse = "_"), "_",
                  format(Sys.time(),'%d-%m-%Y.%H.%M'))  
 
-# plot # degs
+
+p1_data <- significantResultsUnfaceted %>%
+    mutate(facet_contrast = factor(paste0(facet, ": ", contrast), levels = ordered_levels))
+
 if (length(facets) < 10) {
-  p1 = ggplot(significantResultsUnfaceted, aes(x=paste0(facet,": ",contrast))) +
-    geom_bar(aes(y=..count.., fill=facet)) +
-    theme_bw() +
-    theme(axis.text.x = element_text(angle=90, hjust=1)) +
-    ylab("Number of DEGs") +
-    xlab("Facet: contrast")
+    p1 = ggplot(p1_data, aes(x=facet_contrast)) +
+        geom_bar(aes(y=..count.., fill=facet)) +
+        theme_bw() +
+        theme(axis.text.x = element_text(angle=90, hjust=1)) +
+        ylab("Number of DEGs") +
+        xlab("Facet: contrast")
 } else {
-  p1 = ggplot(significantResultsUnfaceted, aes(x=paste0(facet,": ",contrast))) +
-    geom_bar(aes(y=..count.., fill=facet)) +
-    theme_bw() +
-    theme(axis.text.x = element_blank(),
-          legend.position = "none") +
-    facet_wrap(~facet, scales = "free_x") +
-    ylab("Number of DEGs") +
-    xlab("Facet: contrast")
+    p1 = ggplot(p1_data, aes(x=facet_contrast)) +
+        geom_bar(aes(y=..count.., fill=facet)) +
+        theme_bw() +
+        theme(axis.text.x = element_blank(),
+            legend.position = "none") +
+        facet_wrap(~facet, scales = "free_x") +
+        ylab("Number of DEGs") +
+        xlab("Facet: contrast")
 }
-ggsave(file.path(report_dir,paste0(prefix,"_","DEG_summary_plot.png")),p1)
+ggsave(file.path(paths$RData,paste0(prefix,"_","DEG_summary_plot.png")),p1)
 
 
 # plot filter stats
-filtered_table_long <- filtered_table %>%
+p2_data <- filtered_table %>%
   group_by(facet,contrast) %>%
   mutate(not_significant = initial-(relevance_filtered+quantile_filtered+spike_filtered)) %>%
+  mutate(contrast = str_replace(contrast, "_vs_", " vs ")) %>%
   ungroup() %>%
   pivot_longer(cols=c(not_significant,relevance_filtered,quantile_filtered,spike_filtered,passed_all_filters)) %>%
   mutate(perc = value/initial) %>%
-  mutate(name = factor(name, levels=c("relevance_filtered", "not_significant", "quantile_filtered", "spike_filtered", "passed_all_filters")))
+  mutate(name = factor(name, levels=c("relevance_filtered", "not_significant", "quantile_filtered", "spike_filtered", "passed_all_filters"))) %>%
+  mutate(facet_contrast = factor(paste0(facet, ": ", contrast), levels = ordered_levels))
+
 if (length(facets) < 10) {
-  p2 = ggplot(filtered_table_long, aes(x=paste0(facet,": ",contrast),y=value,fill=facet)) +
+  p2 = ggplot(filtered_table_long, aes(x=facet_contrast,y=value,fill=facet)) +
     theme_bw() +
     geom_bar(stat="identity",position="dodge") +
     facet_wrap(~name, scales="free") +
@@ -96,7 +106,7 @@ if (length(facets) < 10) {
     ylab("Percent of all reads") +
     xlab("Facet: contrast")
 } else {
-  p2 = ggplot(filtered_table_long, aes(x=paste0(facet,": ",contrast),y=value,fill=facet)) +
+  p2 = ggplot(filtered_table_long, aes(x=facet_contrast,y=value,fill=facet)) +
     theme_bw() +
     geom_bar(stat="identity",position="dodge") +
     facet_wrap(~name, scales="free") +
