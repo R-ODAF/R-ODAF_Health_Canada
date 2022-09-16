440174d4a1d742dee99f260f387e59d3dc076891
mattjmeier <matthew.james.meier@gmail.com>
2022-09-16 15:59:49 -0400

    Prevent possibly unnecessary 60 second sleeps...

--- scripts/render_DESeq2_report.parallel.R
+++ scripts/render_DESeq2_report.parallel.R
@@ -201,16 +201,14 @@ if(is.na(params$group_facet) && is.na(params$display_group_facet)){
   stop("Making a single report for faceted data not supported. Did you forget to set display_group_facet?")
 }
 
-
-
 if (params$parallel){
   biocluster <- BiocParallel::MulticoreParam(workers = round(params$cpus*0.9))
   BiocParallel::bpmapply(FUN = make_main_reports, facet = facets, MoreArgs = list(pars = params), BPPARAM = biocluster)
-  Sys.sleep(60)
+  if (params$generate_main_report == T) {Sys.sleep(60)}
   BiocParallel::bpmapply(FUN = make_data_reports, facet = facets, MoreArgs = list(pars = params), BPPARAM = biocluster)
-  Sys.sleep(60)
+  if (params$generate_data_explorer_report == T) {Sys.sleep(60)}
   BiocParallel::bpmapply(FUN = make_pathway_reports, facet = facets, MoreArgs = list(pars = params), BPPARAM = biocluster)
-  Sys.sleep(60)
+  if (params$generate_go_pathway_report == T) {Sys.sleep(60)}
   # Why does this use so much memory!?
   # It is knitr::kable that is the problem.
   # See the extra stats Rmd for details, the chunk name metadata-report
