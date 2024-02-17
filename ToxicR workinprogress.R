7369bfcec5e9aa0c8bd9edd57b489f6ba67f4080
Syed Abdullah Syeddan <syedasyeddan@gmail.com>
2024-02-17 18:33:39 -0500

    - add maximum cores to mclapply, - calculate all model and choose best method (based on The maximum value of the likelihod/posterior) also in parellel processing, - input best model - perform williams trend test with alternative both greater and less - start of rmarkdown file using shiny (has not been updated for change in including best model type)

--- ToxicR workinprogress.R
+++ ToxicR workinprogress.R
@@ -6,6 +6,9 @@ library("dplyr")
 library("PMCMRplus")
 library(parallel)
 library("pbmcapply")
+library(foreach)
+library(doParallel)
+
 #bmdoutput <- read.table("~/storage/biomarker_input_.txt",
 #                        header=FALSE, sep = "\t", row.names = 1)
 bmdoutput <- read.table("~/storage/ToxicR mod/data for BMDExpress log2 CPM Males.txt",
@@ -21,12 +24,41 @@ bmdoutput[-1] <- lapply(bmdoutput[-1], function(x) {
 
 dose_column <-as.numeric(bmdoutput[,1]) 
 start.time <- Sys.time()
+
 Func <- function(resp_bygene_column){ 
-  trendtestres<- williamsTest(resp_bygene_column ~ dose_column, alternative = c("greater", "less"))
-  trendestresdf <- data_frame("pass/fail value" = trendtestres$crit.value[,1] - trendtestres$statistic[,1]) 
-  #only generate model when significant based on williams test 
-  if (any(trendestresdf$`pass/fail value` <0)){ 
-    model<-single_continuous_fit(dose_column,resp_bygene_column)
+  trendtestresgreater<- williamsTest(resp_bygene_column ~ dose_column, alternative = "greater")
+  trendtestresdfgreater <- data_frame("pass/fail value" = trendtestresgreater$crit.value[,1] - trendtestresgreater$statistic[,1]) 
+  
+  trendtestresless<- williamsTest(resp_bygene_column ~ dose_column, alternative = "less")
+  trendtestresdfless <- data_frame("pass/fail value" = trendtestresgreater$crit.value[,1] - trendtestresgreater$statistic[,1])
+
+  trendtestresdf <- rbind(trendtestresdfgreater, trendtestresdfless)
+  
+    #only generate model when significant based on williams test 
+  if (any(trendtestresdf$`pass/fail value` <0)){ 
+    
+    #cl <- makeCluster(detectCores())
+    #registerDoParallel(cl)
+    #clusterEvalQ(cl, {
+    #  library("ToxicR")
+    #})
+    #clusterExport(cl, c("dose_column"))
+    modeltypes = c("hill","exp-3","exp-5","power","polynomial")
+    models <- foreach(model_type = modeltypes) %dopar% {
+      single_continuous_fit(dose_column, resp_bygene_column, model_type = model_type)
+    }
+    #stopCluster(cl)
+    
+    
+    
+    #runmodels <- function(modeltype){ 
+    #  single_continuous_fit(dose_column, resp_bygene_column, model_type = modeltype)
+    #}
+    #models <- pbmclapply(modeltypes,runmodels, mc.cores = detectCores())
+    
+    modeloptions <- sapply(models, function(model) model$maximum)
+    index_of_best <- which.max(modeloptions)
+    model <- sapply(models[[index_of_best]], function(model) model)
     #capture p-value output from ToxicR for goodness of fit
     modeloutput<- capture.output(summary(model))
     all_numbers_mean_adequate_pvalue <- lapply(modeloutput[9:10], function(line) {
@@ -34,18 +66,22 @@ Func <- function(resp_bygene_column){
     })
     #only return model if goodness of fit is significant 
     if(!any(as.data.frame(all_numbers_mean_adequate_pvalue) >0.05)){ 
-      return(as.numeric(model$bmd[1]))
+      return(list(as.numeric(model$bmd[1]),model$model))
     }
   }
 }
 
-result <- pbmclapply(bmdoutput[,2:ncol(bmdoutput)], function(col) {
-  return(Func(col))
-})
+result <-pbmclapply(bmdoutput[,2:ncol(bmdoutput)], Func, mc.cores = detectCores())
 
+filtered_list <- Filter(Negate(is.null), result)
+BMDs <- lapply(filtered_list, function(gene) as.numeric(gene[1]))
+modeltypesresults <- lapply(filtered_list, function(gene) as.character(gene[2]))
 #structuring of results to remove NULLs and have rows for output
-signficant_BMD_table <- t(as.data.frame(Filter(Negate(is.null), result)))
+significant_BMD_table <- data.frame("Gene" = names(filtered_list), 
+                                    "BMD" = unlist(BMDs),
+                                    "Modeltype" = unlist(modeltypesresults),row.names = NULL)
+
 end.time <- Sys.time()
 time.taken <- round(end.time - start.time,2)
 time.taken
-write.table(signficant_BMD_table, file ="~/storage/toxicR output.txt", sep=" ", col.names = TRUE)
+write.table(significant_BMD_table, file ="~/storage/toxicR output.txt", sep=" ", col.names = TRUE)
