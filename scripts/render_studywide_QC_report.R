b97aae0e07c94966719ceb9b1d9e421ecdfa024f
mattjmeier <matthew.james.meier@gmail.com>
2024-03-17 16:28:09 -0400

    attempt to get QC report updated

--- scripts/render_studywide_QC_report.R
+++ scripts/render_studywide_QC_report.R
@@ -1,27 +1,13 @@
 #!/usr/bin/R
 # Custom parameters for the report
-library(tidyverse)
-require(yaml)
+library(R.ODAF.utils)
 
-source(here::here("scripts","setup_functions.R"))
+# Sys.setenv(RSTUDIO_PANDOC="/usr/lib/rstudio/bin/pandoc")
 
-#Sys.setenv(RSTUDIO_PANDOC="/usr/lib/rstudio/bin/pandoc")
-
-config <- yaml::read_yaml(here::here("inputs","config","config.yaml"), eval.expr = T)
-
-# Combine required params from config
-params <- c(config$common, config$QC)
-# replace nulls in params with NA
-params <- replace_nulls_in_config(params)
-# If projectdir is not set, figure out current project root directory
-projectdir <- params$projectdir
-if (is.na(projectdir)) {
-  projectdir <- here::here()
-  params$projectdir <- projectdir
-}
+params <- R.ODAF.utils::get_params(context = "QC")
 
 # Input file - Rmd
-input_file <- file.path(projectdir, "Rmd", "Sample_QC.Rmd")
+input_file <- file.path(params$projectdir, "Rmd", "Sample_QC.Rmd")
 
 message("Writing QC report for all samples in the experiment.")
 # Output file - HTML
@@ -30,7 +16,7 @@ filename <- paste0("Study-wide_Sample_QC_",
                    params$project_title, "_",
                    format(Sys.time(),'%d-%m-%Y.%H.%M'),
                    ".html")
-out_dir <- file.path(projectdir, "output", "QC")
+out_dir <- file.path(params$projectdir, "output", "QC")
 if (!dir.exists(out_dir)) {
   dir.create(out_dir, recursive = TRUE)
 }
