71f9f8e4dca7db4e3eefc13217f4f69a696a99d7
laurenbradford <lrn.bradford@gmail.com>
2026-02-20 11:41:02 -0500

    fix postdeploy issue in github workflow

--- workflow/envs/reports.post-deploy.sh
+++ workflow/envs/reports.post-deploy.sh
@@ -3,10 +3,11 @@ set -o pipefail
 
 # This script runs after the reports environment is installed
 # And adds the crosstools package, which isn't available through conda
-conda activate -p $CONDA_PREFIX
 
 mkdir -p output/.rodaf_internal/install_logs
 
-(test -f output/.rodaf_internal/install_logs/myRscript.post-deploy.log || rm output/.rodaf_internal/install_logs/myRscript.post-deploy.log)
+# remove log file if it already exists
+rm -f output/.rodaf_internal/install_logs/install_from_github.post-deploy.log # Changed filename to match your redirect target.
 
-$CONDA_PREFIX/bin/Rscript workflow/envs/install_from_github.R >> output/.rodaf_internal/install_logs/install_from_github.post-deploy.log 2>&1
+# Execute Rscript directly from the CONDA_PREFIX bin directory.
+"$CONDA_PREFIX"/bin/Rscript workflow/envs/install_from_github.R >> output/.rodaf_internal/install_logs/install_from_github.post-deploy.log 2>&1
