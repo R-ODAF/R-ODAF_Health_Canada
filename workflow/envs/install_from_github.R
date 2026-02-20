ffa47bc7ecb7156c961c0e6e9167700f96dde671
laurenbradford <lrn.bradford@gmail.com>
2026-02-20 14:12:05 -0500

    stupid quotes

--- workflow/envs/install_from_github.R
+++ workflow/envs/install_from_github.R
@@ -1,3 +1,3 @@
 # Install extra dependencies
 chooseCRANmirror(1, graphics = FALSE)
-devtools::install_github("bwlewis/crosstool", upgrade=never)
+devtools::install_github("bwlewis/crosstool", upgrade="never")
