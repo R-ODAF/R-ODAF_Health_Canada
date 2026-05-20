16a6803a71444cc6def3333317e02a58efe49217
laurenbradford <lrn.bradford@gmail.com>
2026-05-20 11:29:42 -0400

    output more dedup stats

--- scripts/dedup_stats.R
+++ scripts/dedup_stats.R
@@ -4,7 +4,95 @@
 # Ex. Rscript dedup_stats.R L1306H03_umiDedup-1MM_Directional.tsv L1306H03_umiDedup-No-dedup.tsv output/QC
 
 library(tidyverse)
+library(matrixStats)# This script takes in a deduplicated count table (arg1), a matching non-deduplicated count table (arg2), and an output directory
+# And calculates the ratio of deduplication for each gene in each sample
+# File names must be {library}_umiDedup-{method}.tsv
+# Ex. Rscript dedup_stats.R L1306H03_umiDedup-1MM_Directional.tsv L1306H03_umiDedup-No-dedup.tsv output/QC
+
+library(tidyverse)
 library(matrixStats)
+
+args <- commandArgs(trailingOnly = TRUE)
+
+if (length(args) == 0) {
+  stop("Argument must be supplied: deduplicated count table, non-deduplicated count table.", call. = FALSE)
+}
+
+outdir <- args[3]
+
+
+# Get info from file name
+fname <- basename(args[1])
+library <- str_extract(fname, "^[^_]+")
+umimethod <- str_extract(fname, "(?<=umiDedup-)[^.]+")
+
+outfile_max <- paste0(outdir, "/", library, "_", umimethod, "_dedup_max_ratios.txt")
+outfile_hist <- paste0(outdir, "/", library, "_", umimethod, "_dedup_ratios_hist.png")
+outfile_sums <- paste0(outdir, "/", library, "_", umimethod, "_dedup_countsums.txt")
+
+print(paste0("Library: ", library))
+print(paste0("UMI Method: ", umimethod))
+
+# Import count tables
+dedup <- read.delim(args[1], sep = "\t", header = TRUE, row.names = 1)
+nodedup <- read.delim(args[2], sep = "\t", header = TRUE, row.names = 1)
+
+# Sanity checks
+stopifnot(nrow(dedup) == nrow(nodedup))
+stopifnot(ncol(dedup) == ncol(nodedup))
+stopifnot(all(rownames(dedup) == rownames(nodedup)))
+stopifnot(all(colnames(dedup) == colnames(nodedup)))
+
+# Sum counts for each sample
+df_sums <- data.frame(colSums(dedup), colSums(nodedup)) %>%
+  dplyr::rename(sumreads_dedup = colSums.dedup.,
+                sumreads_nodedup = colSums.nodedup.) %>%
+  dplyr::mutate(ratio = sumreads_dedup/sumreads_nodedup,
+                diff = sumreads_nodedup - sumreads_dedup)
+
+write.table(df_sums, outfile_sums, row.names = FALSE, quote = FALSE, sep = "\t")
+
+# Convert to long format
+dedup_long <- dedup %>%
+  rownames_to_column("gene") %>%
+  pivot_longer(-gene, names_to = "sample", values_to = "dedup_counts")
+
+nodedup_long <- nodedup %>%
+  rownames_to_column("gene") %>%
+  pivot_longer(-gene, names_to = "sample", values_to = "nodedup_counts")
+
+# Join and calculate ratio
+ratio_long <- dedup_long %>%
+  inner_join(nodedup_long, by = c("gene", "sample")) %>%
+  mutate(
+    ratio = nodedup_counts / dedup_counts,
+    ratio = ifelse(is.infinite(ratio) | is.nan(ratio), 0, ratio)
+  )
+
+# Convert back to wide format 
+ratio <- ratio_long %>%
+  select(gene, sample, ratio) %>%
+  pivot_wider(names_from = sample, values_from = ratio, id_cols = gene) %>%
+  column_to_rownames("gene")
+
+# Max ratio of nodedup to dedup per sample
+maxratio_stats <- tibble(
+  sample = colnames(ratio),
+  max_ratio = colMaxs(as.matrix(ratio), na.rm = TRUE)
+) %>%
+  arrange(sample) %>%
+  as.data.frame()
+
+write.table(maxratio_stats, outfile_max, row.names = FALSE, quote = FALSE, sep = "\t")
+
+# Plot deduplication ratios
+
+p <- ggplot(ratio_long, aes(x = ratio)) +
+  geom_histogram(binwidth = 1) +
+  facet_wrap(~sample)
+
+ggsave(outfile_hist, p)
+
 
 args <- commandArgs(trailingOnly = TRUE)
 
